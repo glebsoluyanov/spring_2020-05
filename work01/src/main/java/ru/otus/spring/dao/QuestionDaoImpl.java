4ff91e9464627c7f89b46476d2ae7636a5f8f645
GL <glebsoluyanov@live.ru>
2020-05-31 17:44:58 +0300

    work-01

---
+++ work01/src/main/java/ru/otus/spring/dao/QuestionDaoImpl.java
@@ -0,0 +1,37 @@
+package ru.otus.spring.dao;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import ru.otus.spring.domain.Question;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class QuestionDaoImpl implements QuestionDao {
+
+    public List<Question> getQuestions(String path) throws IOException {
+        InputStream is = this.getClass().getResourceAsStream(path);
+        List<Question> questionList = new ArrayList<>();
+
+        try(
+                InputStreamReader reader = new InputStreamReader(is);
+                CSVParser csvParser = new CSVParser(reader, CSVFormat.EXCEL.withDelimiter(';'));
+        ){
+            List<CSVRecord> recordList = csvParser.getRecords();
+            for(CSVRecord record : recordList){
+                Question question = new Question();
+                question.setQuestion(record.get(0));
+                question.setAnswer(record.get(1));
+                questionList.add(question);
+            }
+        }
+
+        return questionList;
+    }
+}
---
+++ work01/src/test/java/ru/otus/spring/dao/QuestionDaoTest.java
@@ -0,0 +1,17 @@
+package ru.otus.spring.dao;
+
+import org.junit.Test;
+import ru.otus.spring.domain.Question;
+
+import java.io.IOException;
+import java.util.List;
+
+
+public class QuestionDaoTest {
+    @Test
+    public void getQuestionsTest() throws IOException {
+        QuestionDao questionDao = new QuestionDaoImpl();
+        List<Question> questions = questionDao.getQuestions("/questions.csv");
+
+    }
+}
